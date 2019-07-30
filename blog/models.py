9b4fc6f0aa0d1e5ff4348abbdda956b55ded7cb5
unknown <mukulbarai82@gmail.com>
2019-07-30 23:33:42 +0600

    added category properly

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, date
 
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	def __str__(self): return self.name
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+	def __str__(self): return self.name
+
 class Post(models.Model):
 	content = models.CharField(max_length=1000)
-	title = models.CharField(max_length=100, null=True)
+	title = models.CharField(max_length=100, 
+		default='The title goes here')
 	author = models.ForeignKey(User, 
 		on_delete=models.CASCADE,related_name='post')
 	image = models.CharField(max_length=1000)
+	category = models.ForeignKey(Category,
+		on_delete=models.CASCADE, related_name='category')
 	published = models.DateField(default=date.today)
-
+	tags = models.CharField(max_length=100, null=True)
+	def __str__(self): return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 def index(request):
 	posts = Post.objects.all()
-	context = {'posts': posts}
+	categories = Category.objects.all()
+	context = {'posts': posts, 
+		'categories': categories}
 	return render(request, 
-		'index.html', context
-	)
+		'index.html', context)
