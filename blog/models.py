d068ef564dd1a56cb1ef08908802df793fdfd812
unknown <mukulbarai82@gmail.com>
2019-07-30 21:30:09 +0600

    added real data from models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+class Post(models.Model):
+	content = models.CharField(max_length=1000)
+	title = models.CharField(max_length=100, null=True)
+	author = models.ForeignKey(User, 
+		on_delete=models.CASCADE,related_name='post')
+	image = models.CharField(max_length=1000)
+	published = models.DateField(default=date.today)
+
