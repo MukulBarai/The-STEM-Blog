44744eb0b054a6de18092d5bce65b00fbc725ea5
unknown <mukulbarai82@gmail.com>
2019-07-31 16:31:26 +0600

    added codes for admin panel

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('<int:id>/', views.singlePost, name='singlepost')
+	path('posts/<int:id>/', views.singlePost, name='singlepost'),
+	path('site/admin/', views.siteAdmin, name='siteadmin'),
+	path('site/admin/posts/', views.posts, name='posts'),
+	path('site/admin/posts/new', views.newPost, name='newpost')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Category, Menu
 # Create your views here.
 
@@ -24,3 +24,42 @@ def singlePost(request, id):
 	}
 	return render(request, 
 		'posts/single.html', context)
+
+def siteAdmin(request):
+	if not request.user.is_authenticated:
+		return redirect('index')
+	posts = Post.objects.all()
+	categories = Category.objects.all()
+	menus = Menu.objects.all()
+	context = {
+		'posts': posts, 
+		'categories': categories,
+		'menus': menus
+	}
+	return render(request, 'admin/posts.html', context)
+
+def posts(request):
+	if not request.user.is_authenticated:
+		return redirect('index')
+	posts = Post.objects.all()
+	categories = Category.objects.all()
+	menus = Menu.objects.all()
+	context = {
+		'posts': posts, 
+		'categories': categories,
+		'menus': menus
+	}
+	return render(request, 
+		'admin/posts.html', context)
+def newPost(request):
+	if not request.user.is_authenticated:
+		return redirect('index')
+	posts = Post.objects.all()
+	categories = Category.objects.all()
+	menus = Menu.objects.all()
+	context = {
+		'posts': posts, 
+		'categories': categories,
+		'menus': menus
+	}
+	return render(request, 'posts/newpost.html', context)
