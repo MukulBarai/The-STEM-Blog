6cc11c2f017f2751599c7a2e64c79903d1d02402
unknown <mukulbarai82@gmail.com>
2019-08-01 18:42:29 +0600

    fixed some bugs

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django import forms
+from datetime import datetime, date
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, 
@@ -17,3 +18,18 @@ class SignUpForm(UserCreationForm):
         	'username', 'first_name', 'last_name', 
         	'email', 'password1', 'password2', 
         )
+
+class PostForm(forms.Form):
+    content = forms.CharField(
+        max_length=1000,
+        widget=forms.Textarea,
+        label='Post Content'
+    )
+    title = forms.CharField(max_length=100, 
+        label='Post Title')
+    image = forms.CharField(max_length=1000,
+        label='Image Url')
+    category = forms.ChoiceField()
+    published = forms.DateField(initial=date.today)
+    tags = forms.CharField(max_length=100)
+    views = forms.IntegerField(initial=0)
