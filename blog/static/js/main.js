3b4b17d77e84dd7ebe21475baf8393427c8ff13c
unknown <mukulbarai82@gmail.com>
2019-08-01 23:35:31 +0600

    changed codes

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
 	$('.form-group input').addClass('form-control');
 	$('.form-group textarea').addClass('form-control');
 	$('.form-group select').addClass('form-control');
+	$('.form-group textarea').attr('row', '5');
 })
