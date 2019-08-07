1e652df092d091f9450d910586c13ecf01d13786
unknown <mukulbarai82@gmail.com>
2019-08-07 15:07:58 +0600

    changed middleware code

--- blog/middleware.py
+++ blog/middleware.py
@@ -2,26 +2,27 @@ from .models import Post, Category, Tag
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-def getArchives():
-    dateFrom = datetime(2018, 1, 1)
-    dateTo = datetime.today()
-
-    archives = []
-    months = [
-        'January', 'February', 'March', 
-        'April', 'May', 'June', 'July', 'August', 
-        'September', 'October', 'November', 'December'
-    ]
-    for year in (dateFrom.year, dateTo.year):
-        for i in range(dateTo.month if year == dateTo.year else 12):
-            archives.append({
-                'year': year, 'month': i+1, 
-                'yearmon': str(year) + " " + months[i]
-            })
-    return archives
-
 
 def basicMiddleware(get_response):
+    def getArchives():
+        dateFrom = datetime(2018, 1, 1)
+        dateTo = datetime.today()
+
+        archives = []
+        months = [
+            'January', 'February', 'March', 
+            'April', 'May', 'June', 'July', 'August', 
+            'September', 'October', 'November', 'December'
+        ]
+        for year in (dateFrom.year, dateTo.year):
+            for i in range(dateTo.month if year == dateTo.year else 12):
+                archives.append({
+                    'year': year, 'month': i+1, 
+                    'yearmon': str(year) + " " + months[i]
+                })
+        return archives
+
+
     def middleware(request):
         posts = Post.objects.order_by('-pk').all()
         popular = Post.objects.order_by('-views')[:20]
